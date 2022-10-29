4ca3b8ea65e88ed26f8fe0380085b993e79b097c
William Puma <dwilgeo95@gmail.com>
2022-10-29 02:37:38 -0500

    env para produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   secret: "BIBLIOBOTUNIVERSIDADTENICADELNORTEIBARRA",
-  // baseUrl: "http://localhost:9095",
-  // hubConnectionURL: 'http://localhost:9095/session',
+  baseUrl: "https://bibliochatservice02.azurewebsites.net",
+  hubConnectionURL: 'https://bibliochatservice02.azurewebsites.net/session',
   apiRol: "/api/rol",
   apiUsuario: "/api/usuario",
   apiCliente: "/api/cliente",
